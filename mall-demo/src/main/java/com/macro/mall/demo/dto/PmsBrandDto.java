69980c52a120b109efa9a5142beca868ce9d5a66
muxiaoxin0221 <knightyyq@gmail.com>
2024-04-12 21:44:10 +0800

    remove FlagValidator

--- mall-demo/src/main/java/com/macro/mall/demo/dto/PmsBrandDto.java
+++ mall-demo/src/main/java/com/macro/mall/demo/dto/PmsBrandDto.java
@@ -1,6 +1,5 @@
 package com.macro.mall.demo.dto;
 
-import com.macro.mall.demo.validator.FlagValidator;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
@@ -10,20 +9,18 @@ import jakarta.validation.constraints.NotNull;
  */
 @Schema(description = "PmsBrandDto")
 public class PmsBrandDto {
-    @Schema(description = "品牌名称", required = true)
+    @Schema(description = "品牌名称")
     @NotNull(message = "名称不能为空")
     private String name;
-    @Schema(description = "品牌首字母", required = true)
+    @Schema(description = "品牌首字母")
     @NotNull(message = "首字母不能为空")
     private String firstLetter;
     @Schema(description = "排序字段")
     @Min(value = 0, message = "排序最小为0")
     private Integer sort;
     @Schema(description = "是否为厂家制造商")
-    @FlagValidator(values = {"0", "1"}, message = "厂家状态不正确")
     private Integer factoryStatus;
     @Schema(description = "是否进行显示")
-    @FlagValidator(values = {"0", "1"}, message = "显示状态不正确")
     private Integer showStatus;
     @Schema(description = "品牌logo")
     private String logo;
