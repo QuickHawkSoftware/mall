ab32976d67f65a79fd41a5a5c096e0f9b9fa1f21
muxiaoxin0221 <knightyyq@gmail.com>
2024-04-25 21:17:20 +0800

    add lombok

--- mall-demo/src/main/java/com/macro/mall/demo/dto/PmsBrandDto.java
+++ mall-demo/src/main/java/com/macro/mall/demo/dto/PmsBrandDto.java
@@ -3,93 +3,39 @@ package com.macro.mall.demo.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
 /**
  * 品牌传递参数
  */
 @Schema(description = "PmsBrandDto")
+@Data
 public class PmsBrandDto {
   @Schema(description = "品牌名称")
   @NotNull(message = "名称不能为空")
   private String name;
+
   @Schema(description = "品牌首字母")
   @NotNull(message = "首字母不能为空")
   private String firstLetter;
+
   @Schema(description = "排序字段")
   @Min(value = 0, message = "排序最小为0")
   private Integer sort;
+
   @Schema(description = "是否为厂家制造商")
   private Integer factoryStatus;
+
   @Schema(description = "是否进行显示")
   private Integer showStatus;
+
   @Schema(description = "品牌logo")
   private String logo;
+
   @Schema(description = "品牌大图")
   private String bigPic;
+  
   @Schema(description = "品牌故事")
   private String brandStory;
 
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getFirstLetter() {
-    return firstLetter;
-  }
-
-  public void setFirstLetter(String firstLetter) {
-    this.firstLetter = firstLetter;
-  }
-
-  public Integer getSort() {
-    return sort;
-  }
-
-  public void setSort(Integer sort) {
-    this.sort = sort;
-  }
-
-  public Integer getFactoryStatus() {
-    return factoryStatus;
-  }
-
-  public void setFactoryStatus(Integer factoryStatus) {
-    this.factoryStatus = factoryStatus;
-  }
-
-  public Integer getShowStatus() {
-    return showStatus;
-  }
-
-  public void setShowStatus(Integer showStatus) {
-    this.showStatus = showStatus;
-  }
-
-  public String getLogo() {
-    return logo;
-  }
-
-  public void setLogo(String logo) {
-    this.logo = logo;
-  }
-
-  public String getBigPic() {
-    return bigPic;
-  }
-
-  public void setBigPic(String bigPic) {
-    this.bigPic = bigPic;
-  }
-
-  public String getBrandStory() {
-    return brandStory;
-  }
-
-  public void setBrandStory(String brandStory) {
-    this.brandStory = brandStory;
-  }
 }
